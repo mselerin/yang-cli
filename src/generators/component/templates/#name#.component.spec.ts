64cbb8d5dde0f8794de60fa073d3f9faa63db4a4
Michel Selerin <michel.selerin@outlook.com>
2018-01-12 16:25:45 +0100

    (fix) Unit Test

--- src/generators/component/templates/#name#.component.spec.ts
+++ src/generators/component/templates/#name#.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { <%=classify_name%>Component } from './<%=dasherize_name%>.component';
+import {CoreModule} from '../../core/core.module';
+import {SharedModule} from '../../shared/shared.module';
 
 describe('<%=classify_name%>Component', () => {
     let component: <%=classify_name%>Component;
@@ -8,6 +10,7 @@ describe('<%=classify_name%>Component', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [ CoreModule, SharedModule ],
             declarations: [ <%=classify_name%>Component ]
         })
             .compileComponents();
