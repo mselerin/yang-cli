9516ed1e3b1f86ad0a6fbf4380b91dfae7025160
Michel Selerin <michel.selerin@outlook.com>
2018-05-12 00:24:40 +0200

    fix: detection of schematic name with yang plugin

--- src/commands/yang-plugin.command.ts
+++ src/commands/yang-plugin.command.ts
@@ -1,16 +1,25 @@
 import { YangCommand } from "./yang.command";
 import { YangUtils } from '../helpers/yang-utils';
+import chalk from 'chalk';
 
 export class YangPluginCommand extends YangCommand
 {
     async run(options: any = {}): Promise<void> {
         let argv = [...process.argv].slice(2);
         let schematicName: string = argv[0];
+        if (!schematicName) {
+            console.log(chalk.bgRed(`No schematic name provided`));
+            process.exit(1);
+        }
+
         let fullSchematicName: string = schematicName;
         if (schematicName.startsWith('@')) {
             fullSchematicName = schematicName.substring(0, schematicName.lastIndexOf('/'))
                 + '/yang-schematics:'
                 + schematicName.substring(schematicName.lastIndexOf('/') + 1);
+        }
+        else {
+            fullSchematicName = 'yang-schematics:' + schematicName;
         }
 
         YangUtils.spawnCommandSync('ng', ['g', fullSchematicName, ...argv]);
