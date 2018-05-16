e259398ab9f174ef93bf2245da13123043655aec
Michel Selerin <michel.selerin@outlook.com>
2018-05-16 23:34:13 +0200

    feat: new command based on the "ng-new" schematic from yang-schematics

--- src/commands/yang-new.command.ts
+++ src/commands/yang-new.command.ts
@@ -10,6 +10,7 @@ export class YangNewCommand extends YangCommand
 {
     static yargs(yargs: Argv): Argv {
         return super.yargs(yargs)
+            .option('install', { type: 'boolean', default: false, describe: 'Install dependencies' })
             .option('skip-git', { type: 'boolean', default: false, describe: 'Skip git' })
         ;
     }
@@ -22,35 +23,17 @@ export class YangNewCommand extends YangCommand
         }
 
         const name = options['name'] || path.basename(process.cwd());
-        const dir = `./${name}`;
         const skipGit = options['skip-git'] || false;
+        const skipInstall = !options['install'] || true;
 
-        let ngOpts = ['new', name
-            , '--style', 'scss'
-            , '--inline-style'
-            , '--inline-template'
-            , '--skip-install'
-        ];
+        let ngOpts = ['new', name, '--collection', 'yang-schematics'];
 
         if (skipGit)
             ngOpts.push('--skip-git');
 
+        if (skipInstall)
+            ngOpts.push('--skip-install');
+
         YangUtils.runNgCli(ngOpts);
-
-        // Call yang-schematics:init
-        YangUtils.runNgCli(['g', 'yang-schematics:init'], {cwd: dir});
-
-
-        // git commit
-        if (!skipGit) {
-            if (commandExists('git')) {
-                let spawnOpts = { cwd: dir, stdio: 'ignore' };
-                YangUtils.spawnCommandSync('git', ['add', '.'], spawnOpts);
-                YangUtils.spawnCommandSync('git', ['commit', '-m', 'refactor: Yang Initialization'], spawnOpts);
-            }
-        }
-
-        if (options.install)
-            YangUtils.spawnCommandSync("yarn", [], { cwd: dir});
     }
 }
