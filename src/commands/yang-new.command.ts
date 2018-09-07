7c04468f2df96aeb69ed0b6c7aad8924b64f88c6
Michel Selerin <michel.selerin@outlook.com>
2018-09-07 14:15:08 +0200

    refactor: simplify the new command

--- src/commands/yang-new.command.ts
+++ src/commands/yang-new.command.ts
@@ -1,18 +1,8 @@
 import { YangCommand } from './yang.command';
-import * as path from 'path';
-import { Argv } from 'yargs';
 import { YangUtils } from '../helpers/yang-utils';
 
 export class YangNewCommand extends YangCommand
 {
-  static yargs(yargs: Argv): Argv {
-    return super.yargs(yargs)
-      .option('install', { type: 'boolean', default: false, describe: 'Install dependencies' })
-      .option('skip-git', { type: 'boolean', default: false, describe: 'Skip git' })
-      ;
-  }
-
-
   async run(options: any = {}): Promise<void> {
     if (!await YangUtils.ensurePackage('@angular/cli', options['quiet'])) {
       process.exit(1);
@@ -24,17 +14,8 @@ export class YangNewCommand extends YangCommand
       return;
     }
 
-    const name = options['name'] || path.basename(process.cwd());
-    const skipGit = options['skip-git'] || false;
-    const skipInstall = !options['install'] || true;
-
-    let ngOpts = ['new', name, '--directory', name, '--collection', 'yang-schematics'];
-
-    if (skipGit)
-      ngOpts.push('--skip-git');
-
-    if (skipInstall)
-      ngOpts.push('--skip-install');
+    const ngOpts = [...YangUtils.prepareNgOptions(), '--collection', 'yang-schematics'];
+    const name = ngOpts.shift();
 
     YangUtils.runNgCli('new', name, ngOpts);
   }
